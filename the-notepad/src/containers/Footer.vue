46fac34aacc65c320d988ad0623b7922acac9964
kerbethecoder <krbycnts@gmail.com>
2023-11-21 14:58:02 +0800

    migration to nextjs

--- the-notepad/src/containers/Footer.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <div>Footer</div>
-</template>
-
-<style scoped></style>
--- the-notepad/src/containers/Lessons.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <div>Lessons</div>
-</template>
-
-<style scoped></style>
