0cf6112115e6a478733f7149a550694f7f346f6b
kerbethecoder <krbycnts@gmail.com>
2023-12-02 19:55:29 +0800

    finished dev page

--- app/developer/page.jsx
+++ app/developer/page.jsx
@@ -1,3 +1,121 @@
+import Image from 'next/image';
+import { FaGithub, FaArrowUpRightFromSquare } from 'react-icons/fa6';
+
+import { viga, inter } from '@/ui/fonts';
+import { styles } from '@/ui/style';
+
 export default function Developer() {
-  return <main>developer</main>;
+  return (
+    <main className="my-28">
+      <div id="dev-section" className={`${styles.flexCenter} gap-48`}>
+        <Image src="/dev-img.png" alt="dev-avatar" width={200} height={100} />
+        <div>
+          <h1 className={`${viga.className} text-4xl`}>Meet the Developer!</h1>
+          <p>
+            <strong>@kerbethecoder</strong>, the man behind this small website.
+            The idea started when <br /> the developer finds it hard to check
+            from time to time for his notes or in the internet <br /> regarding
+            different matters such as searching for color palettes or font
+            pairings, or <br /> even for quick-starts whenever he decides to
+            start on a new project.
+          </p>
+          <p className="mt-10 text-sm font-bold">
+            To know more about him, check the links in the footer. ^^
+          </p>
+        </div>
+      </div>
+
+      <div className={`${styles.flexCenter} my-28`}>
+        <Image src="/hr.svg" alt="horizontal rule" width={1100} height={1} />
+      </div>
+
+      <div id="proj-section" className={`${styles.flexCenter} flex-col`}>
+        <Image
+          src="/dev-projs.png"
+          alt="projects-vector-art"
+          width={450}
+          height={100}
+        />
+        <p className="py-10">
+          This section is dedicated to the developer&#39;s sample projects if
+          you are interested.
+        </p>
+
+        <div className={`${styles.flexCenter} gap-5`}>
+          <div
+            className="w-1/4 outline-drkgry outline outline-2 rounded-md p-5 flex gap-8 flex-col transition duration-300 ease-in-out hover:scale-105"
+            id="proj1">
+            <div className={`${styles.flexBetween}`}>
+              <span
+                className={`${viga.className} text-rd text-3xl strk-outline`}>
+                #1
+              </span>
+              <div className="flex gap-3">
+                <a
+                  href="https://github.com/kerbethecoder/rock-paper-scissors"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition duration-300 ease-in-out hover:scale-125">
+                  <FaGithub size="1.7em" />
+                </a>
+                <a
+                  href="https://game-rps.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition duration-300 ease-in-out hover:scale-125">
+                  <FaArrowUpRightFromSquare size="1.7em" />
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <h3 className={`${inter.className} font-bold mb-1`}>
+                ROCK, PAPER, SCISSORS
+              </h3>
+              <p className="text-sm">
+                Simple game to play with friends vs computer. Project showcasing
+                skills with html, css, and vanilla JavaScript.
+              </p>
+            </div>
+          </div>
+          <div
+            className="w-1/4 outline-drkgry outline outline-2 rounded-md p-5 flex gap-8 flex-col transition duration-300 ease-in-out hover:scale-105"
+            id="proj2">
+            <div className={`${styles.flexBetween}`}>
+              <span
+                className={`${viga.className} text-blgrn text-3xl strk-outline`}>
+                #2
+              </span>
+              <div className="flex gap-3">
+                <a
+                  href="https://github.com/kerbethecoder/netflix-react"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition duration-300 ease-in-out hover:scale-125">
+                  <FaGithub size="1.7em" />
+                </a>
+                <a
+                  href="https://mock-netflix-react.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition duration-300 ease-in-out hover:scale-125">
+                  <FaArrowUpRightFromSquare size="1.7em" />
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <h3 className={`${inter.className} font-bold mb-1`}>
+                NETFLIX MOCK LANDING PAGE
+              </h3>
+              <p className="text-sm">
+                Mock landing page of the streaming platform Netflix to practice
+                my front-end web development skills using React.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -17,13 +17,10 @@ export default function Home() {
           <h1 className={`${viga.className} text-5xl`}>The NotePad.</h1>
           <p className="my-2 text-lg">
             The notepad is a personal repository of tools, lessons, and other
-            things that
-            <br />
-            helps on the developer&#39;s transformative journey, beginning with
-            the aspiration
-            <br />
-            of just being a front-end web developer to becoming a full-stack web
-            <br /> developer in the future.
+            things that <br /> helps on the developer&#39;s transformative
+            journey, beginning with the aspiration <br /> of just being a
+            front-end web developer to becoming a full-stack web <br />{' '}
+            developer in the future.
           </p>
           <div className="flex mt-10 ml-16 gap-5">
             <Link href="/developer">
@@ -55,15 +52,11 @@ export default function Home() {
         </h3>
         <p className="text-center">
           Explore a developer's curated collection of recent color palettes,
-          offering a visual journey through the
-          <br />
-          harmonious and eye-catching color schemes used in their previous
-          projects. From vibrant web designs to
-          <br />
-          sleek mobile apps, this page showcases the artistry of color selection
-          in coding, providing inspiration for
-          <br />
-          your own creative endeavors.
+          offering a visual journey through the <br /> harmonious and
+          eye-catching color schemes used in their previous projects. From
+          vibrant web designs to <br /> sleek mobile apps, this page showcases
+          the artistry of color selection in coding, providing inspiration for{' '}
+          <br /> your own creative endeavors.
         </p>
         <Link href="/color-palettes" className="mt-10">
           <Button
@@ -87,15 +80,12 @@ export default function Home() {
           <h3 className={`${viga.className} text-lg mb-2.5`}>FONT PAIRINGS</h3>
           <p className="mb-2.5">
             Discover a dedicated page showcasing a developer's recent font
-            pairings from
-            <br /> their previous projects. Explore the synergy of typefaces,
-            their balance, and the
-            <br /> aesthetic appeal in various applications, including websites,
-            mobile apps, or
-            <br /> print materials. Whether you seek design inspiration or
-            practical guidance, this
-            <br /> collection offers insights into the world of typography and
-            design.
+            pairings from <br /> their previous projects. Explore the synergy of
+            typefaces, their balance, and the <br /> aesthetic appeal in various
+            applications, including websites, mobile apps, or <br /> print
+            materials. Whether you seek design inspiration or practical
+            guidance, this <br /> collection offers insights into the world of
+            typography and design.
           </p>
           <Link href="/font-pairings">
             <Button
@@ -114,15 +104,11 @@ export default function Home() {
           <h3 className={`${viga.className} text-lg mb-2.5`}>JS SNIPPETS</h3>
           <p className="text-right">
             Have a question like those above? Visit the developer&#39;s go-to
-            resource
-            <br /> for essential JavaScript snippets, designed to simplify
-            common tasks in
-            <br /> your projects. Find quick solutions for centering divs,
-            mapping through
-            <br /> arrays, harnessing object keys for efficient mapping, and
-            more. Whether
-            <br /> you&#39;re a seasoned coder or just starting, this collection
-            is your handy
+            resource <br /> for essential JavaScript snippets, designed to
+            simplify common tasks in <br /> your projects. Find quick solutions
+            for centering divs, mapping through <br /> arrays, harnessing object
+            keys for efficient mapping, and more. Whether <br /> you&#39;re a
+            seasoned coder or just starting, this collection is your handy{' '}
             <br /> toolbox for smoother and more productive development.
           </p>
           <Link href="/js-snippets" className="mt-2.5">
@@ -159,14 +145,13 @@ export default function Home() {
         </h3>
         <p className="text-center">
           Explore the developer&#39;s quick start page for front-end
-          development, offering a treasure trove of resources for various
-          <br />
+          development, offering a treasure trove of resources for various <br />{' '}
           technologies such as React, Gatsby, Vue.js, and more. Dive into
-          step-by-step guides and code examples to kickstart your
-          <br /> project swiftly. Discover seamless integrations with popular
-          CSS frameworks like Tailwind CSS, providing with the tools and
-          <br /> knowledge to create stunning, responsive, and feature-rich web
-          applications with ease.
+          step-by-step guides and code examples to kickstart your <br /> project
+          swiftly. Discover seamless integrations with popular CSS frameworks
+          like Tailwind CSS, providing with the tools and <br /> knowledge to
+          create stunning, responsive, and feature-rich web applications with
+          ease.
         </p>
         <Link href="/quick-starts" className="mt-10">
           <Button
